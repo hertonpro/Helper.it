a2a76ccaf0ddb9f590933fe5d88f4f5f42ab250b
hertonpro <alvantyomihigo@gmail.com>
2018-08-09 12:13:23 +0200

    fixe long key rerror

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,11 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-<<<<<<< HEAD
 use Illuminate\Support\Facades\Schema;
-=======
-use Schema;
->>>>>>> bbf9795137d5d597ee0c595b6bc66f9559f86c24
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,11 +15,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
-<<<<<<< HEAD
         Schema::defaultStringLength(191);
-=======
-         Schema::defaultStringLength(191);
->>>>>>> bbf9795137d5d597ee0c595b6bc66f9559f86c24
     }
 
     /**
